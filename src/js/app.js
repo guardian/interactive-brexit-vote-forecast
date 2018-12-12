03147d8877ce34029c4e2edb6093ab3191e6ae3b
Seán Clarke <sean.clarke@guardian.co.uk>
2018-12-12 14:27:49 +0000

    tory confidence variant

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,15 @@ import mustache from 'mustache'
 import axios from 'axios'
 import chartHTML from '!raw-loader!./../templates/chart.html'
 
-const dataurl = "https://interactive.guim.co.uk/docsdata/1lG3HvTwaxzTOl6DcAp_PT-B1_VZmgX7uUMmTveM21o8.json"
+//const dataurl = "https://interactive.guim.co.uk/docsdata/1lG3HvTwaxzTOl6DcAp_PT-B1_VZmgX7uUMmTveM21o8.json"
+const dataurl = "https://interactive.guim.co.uk/docsdata-test/1j7e_fEZi-chBwR0LowmImsMNVEPUM3mFU8VTy_S8FTw.json"
+
 
 const target = document.querySelector('.gv-chart-target')
 
 
 axios.get(dataurl).then(r => {
-    var totals = r.data.sheets.totalsOutput;
+    var totals = r.data.sheets.summary;
     console.log(totals);
     totals.map(p => {
         for (var prop in p) {
@@ -16,11 +18,11 @@ axios.get(dataurl).then(r => {
                 p[prop] = 0;
             } 
         }
+        p.PartyClass = p.Party.replace(/ /g,"");
         p.For = parseInt(p.For);
         p.Against = parseInt(p.Against);
-        p.UnconfirmedFors = parseInt(p.UnconfirmedFors);
-        p.majorFor = p.For > 29 ? true : false;
-        p.majorAgainst = p.Against > 29 ? true : false;
+        p.majorFor = p.For > 19 ? true : false;
+        p.majorAgainst = p.Against > 19 ? true : false;
         p.zeroFor = p.For == 0 ? true : false;
         p.zeroAgainst = p.Against == 0 ? true : false;
     });
@@ -32,26 +34,14 @@ axios.get(dataurl).then(r => {
     totals.forEach(p => {
         totalAgainst = totalAgainst + p.Against;
     } )
-    var totalUnconfirmedFors = 0;
-    totals.forEach(p => {
-        totalUnconfirmedFors = totalUnconfirmedFors + p.UnconfirmedFors;
-    } )
     var largestcategory = totalAgainst > totalFor ? totalAgainst : totalFor;
     totals.map((p,i) => {
         console.log(i);
         p.forWidth = 100 * (p.For / largestcategory);
         p.againstWidth = 100 * (p.Against / largestcategory);
-        p.unconfirmedWidth = 100 * (p.UnconfirmedFors / largestcategory);
-        p.confirmedForWidth = p.forWidth - p.unconfirmedWidth;
-        // if (i = 0) {
-        //     p.againstLeft = 0;
-        // } else {
-        //     p.againstLeft = 
-        // }
-
     })
 
-    var chartoutput = mustache.render(chartHTML, {totals,totalFor,totalAgainst,totalUnconfirmedFors})
+    var chartoutput = mustache.render(chartHTML, {totals,totalFor,totalAgainst})
     target.innerHTML = chartoutput;
 
     window.resize();
